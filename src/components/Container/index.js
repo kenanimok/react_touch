480f6da326389de3e67c0ac2d417cdb052b8d8ea
kenanimok <89970690+kenanimok@users.noreply.github.com>
2022-02-26 16:02:26 +0700

    first commit

---
+++ src/components/Container/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+
+
+
+export default function index(){
+
+     return <Container></Container>
+}
+
+const Container = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+// const  Container =()=>{
+
+
+// // Create a Wrapper component that'll render a <section> tag with some styles
+// const Wrappercon = styled.section`
+//   padding: 4em;
+//   background: papayawhip;
+// `;
+
+// return (
+//     <Wrappercon>
+ 
+//     </Wrappercon>
+// );
+
+
+// }
+// export default Container;
